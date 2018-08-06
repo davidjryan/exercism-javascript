602dc6492b52205699a33bd5d6228f02591a7f1b
David Ryan <djrdev@icloud.com>
2018-08-05 19:16:27 -0600

    two-fer finished

--- two-fer/two-fer.js
+++ two-fer/two-fer.js
@@ -1,12 +1,8 @@
 var TwoFer = function () {};
 
 TwoFer.prototype.twoFer = function (who) {
-  // your code goes here
-  // You will have to use the parameter who
-  // in some way. In this example, it is just
-  // returned, but your solution will have to
-  // use a conditional.
-  return who;
+  
+  return who ? "One for " + who + ", One for me." : "One for you, one for me.";
 };
 
 module.exports = TwoFer;
